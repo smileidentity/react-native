0c6173bb412798d8f1ac705c8946f1265ff6343e
Japhet <jndhlovu54@gmail.com>
2023-08-24 15:32:55 +0200

    feat: pr feedback and package name rename

--- android/src/main/java/com/smileidentity/react/SmileIdPackage.kt
+++ android/src/main/java/com/smileidentity/react/SmileIdPackage.kt
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.module.model.ReactModuleInfo
-import com.smileidentity.BuildConfig
+import com.smileidentity.react.BuildConfig
 import java.util.HashMap
 
 class SmileIdPackage : TurboReactPackage() {
