f9adf702ff64a0291843f6ff42306bcdd1060281
JNdhlovu <jndhlovu54@gmail.com>
2025-02-18 12:19:35 +0200

    chore: remove logs

--- src/useSmileIDView.tsx
+++ src/useSmileIDView.tsx
@@ -28,7 +28,6 @@ export const useSmileIDView = (viewName: string, props: SmileIDProps) => {
           ...prev,
           userId: 'invalidator',
         }));
-        console.log('Japhet Ndhlovu event = ', JSON.stringify(event));
         if (onResultRef.current) {
           const nativeEvent = {
             nativeEvent: {
