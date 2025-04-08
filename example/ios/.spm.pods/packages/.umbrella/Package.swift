d596c96347618bb34cdc8c9d91c3268ee3045089
JNdhlovu <jndhlovu54@gmail.com>
2025-04-08 15:26:33 +0200

    feat: spm test

--- example/ios/.spm.pods/packages/.umbrella/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version:5.7
-import PackageDescription
-
-let package = Package(
-  name: "_umbrella_",
-  dependencies: [
-    .package(url: "https://github.com/smileidentity/ios.git", .upToNextMajor(from: "10.5.1"))
-  ]
-)
