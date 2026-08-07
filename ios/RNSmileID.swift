e05e9a2a47b07d4ac2c6d50c485240961dc25574
Harun Wangereka <harunwangereka@gmail.com>
2026-08-07 16:04:20 +0300

    Prep Release v11.2.0 (#233)
    * chore(release): update version to 11.2.0 and changelog for August 7, 2026
    
    * chore: update Android version to 11.2.0 in gradle.properties

--- ios/RNSmileID.swift
+++ ios/RNSmileID.swift
@@ -11,7 +11,7 @@ class RNSmileID: NSObject {
     resolve: @escaping RCTPromiseResolveBlock,
     reject _: @escaping RCTPromiseRejectBlock
   ) {
-    SmileID.setWrapperInfo(name: .reactNative, version: "11.1.9")
+    SmileID.setWrapperInfo(name: .reactNative, version: "11.2.0")
 
     // Handle different initialization scenarios based on provided parameters
     if let apiKey = apiKey, let config = config {
